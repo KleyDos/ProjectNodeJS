e474bae4bdf8f936ed4d3ccc8421154f733ed3ea
kleyzon <kleyzon@pop-os.localdomain>
2022-08-17 06:27:34 -0600

    second

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+
+// web server
+const app = express()
+
+// middleware
+app.use(express.json())
+app.use(cors())
+app.use(helmet())
+
+// routes
+const router = express.Router()
+router.get('/', function (req, res) {
+
+  res.send('Hola mundo!!!')
+
+})
+router.get("/dos", function(req, res){
+
+  res.send("hola mundodosssss!!!")
+})
+//crear las rutas para el blog
+
+
+app.use(router)
+
+// start server
+app.listen(3001, function () {
+  console.log('Server runnig on opprt 3000')
+})
