bdf6574c45caf8b37c6099a6c57e9ae7c142ad7a
kleyzon <kleyzon@pop-os.localdomain>
2022-08-29 06:52:41 -0600

    29 08 222

--- src/main.js
+++ src/main.js
@@ -57,36 +57,6 @@ router.post('/logout', (req, res) => {
     user.username = ''
     user.fullname = ''
     user.password = ''
-console.log(user);
-    // user = { username: '', fullname: '' }
-
-    return res.send('true')
-  } catch (error) {
-    console.error('error: ', error)
-    return res.send('false')
-  }
-})
-
-router.post("/editar", (req, res) =>{
-  try {
-
-    user.username = req.body.username
-    user.fullname = req.body.fullname
-    console.log('fullname', user.fullname)
-    console.log('username', user.username)
-
-    return res.send("true")
-  } catch (error) {
-    console.error('Error: ', error)
-    return res.send("false")
-  }
-})
-
-router.post('/add', (req, res) => {
-  try {
-    user.username = ''
-    user.fullname = ''
-    user.password = ''
     console.log(user)
     // user = { username: '', fullname: '' }
 
@@ -96,6 +66,64 @@ router.post('/add', (req, res) => {
     return res.send('false')
   }
 })
+
+router.post('/editar', (req, res) => {
+  try {
+    user.username = req.body.username
+    user.fullname = req.body.fullname
+    console.log('fullname', user.fullname)
+    console.log('username', user.username)
+
+    return res.send('true')
+  } catch (error) {
+    console.error('Error: ', error)
+    return res.send('false')
+  }
+})
+
+const blog = { titulo: '', historia: '', autor: '' }
+
+router.post('/add', function (req, res) {
+  try {
+    blog.titulo = req.body.titulo
+    blog.historia = req.body.historia
+    blog.autor = req.body.autor
+    console.log('titulo', blog.titulo)
+    console.log('historia', blog.historia)
+    console.log('autor', blog.autor)
+
+    return res.send('true')
+  } catch (error) {
+    console.log('Error: ', error)
+    return res.send('false')
+  }
+})
+
+router.post('/delete', function (req, res) {
+  try {
+    blog.titulo = req.body.titulo
+    console.log('Post Eliminado', postDelete)
+
+    this.posts = this.posts.filter(post => {
+      console.log('post iteración', post.titulo)
+      if (postDelete !== post.titulo) {
+        return true
+      }
+    })
+
+    console.table(this.posts)
+
+    localStorage.setItem('blog', JSON.stringify(this.posts))
+
+    // localStorage.removeItem("posts", JSON.stringify(this.posts));
+    return true
+  } catch (error) {
+    console.log('Error: ', error)
+    return false
+  }
+})
+
+
 
 app.use(router)
 
